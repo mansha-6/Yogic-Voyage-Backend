a0dc7e9bd38323314c822f38eba55e08e1baeef1
mansha-6 <manshasoni23@gmail.com>
2025-02-27 01:39:58 +0530

    files added

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
